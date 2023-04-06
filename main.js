f6ba4cb54fdbb69d78cfb6343eb5c508ed1829ed
Maxime Coens <maxime.coens@student.hogent.be>
2023-04-07 01:50:55 +0200

    sort by selection algorithm

--- main.js
+++ main.js
@@ -1,50 +1,114 @@
 window.addEventListener('load', () => {
-  const draw = SVG().addTo('#container').size(600, 500);
 
-const arr = [];
-const rectWidth = 30;
-const rectHeight = 30;
-const rectSpacing = 10;
-const rectStartX = 50;
-const rectStartY = 50;
+  async function sortBySelection() {
 
-for (let i = 0; i < 20; i++) {
-  const randomNumber = Math.floor(Math.random() * 100);
-  const rect = draw.rect(rectWidth, rectHeight).move(rectStartX + i * (rectWidth + rectSpacing), rectStartY);
-  rect.fill('#2ecc71');
-  const text = draw.text(randomNumber).move(rectStartX + i * (rectWidth + rectSpacing) + 5, rectStartY + 7);
-  arr.push({value: randomNumber, rect: rect, text: text});
-}
+    const draw = SVG().addTo("#sortBySelection").size(600, 100);
 
-async function bubbleSort() {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j].value > arr[j+1].value) {
-        // Swap rectangles
-        const tempX = arr[j].rect.x();
-        arr[j].rect.x(arr[j+1].rect.x());
-        arr[j+1].rect.x(tempX);
-        
-        // Swap text
-        const tempTextX = arr[j].text.x();
-        arr[j].text.x(arr[j+1].text.x());
-        arr[j+1].text.x(tempTextX);
-        
-        // Swap elements in array
-        const temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
-      }
-      // Color code the rectangles
-      arr[j].rect.fill('#2ecc71');
-      arr[j+1].rect.fill('#e74c3c');
-      await new Promise(r => setTimeout(r, 100)); // Pause for visualization
+    const arr = [];
+    const rectWidth = 30;
+    const rectHeight = 30;
+    const rectSpacing = 10;
+    const rectStartX = 50;
+    const rectStartY = 50;
+
+    for (let i = 0; i < 14; i++) {
+      const randomNumber = Math.floor(Math.random() * 100);
+      const rect = draw.rect(rectWidth, rectHeight).move(rectStartX + i * (rectWidth + rectSpacing), rectStartY);
+      rect.fill('#2ecc71');
+      const text = draw.text(randomNumber).move(rectStartX + i * (rectWidth + rectSpacing) + 5, rectStartY + 7);
+      arr.push({value: randomNumber, rect: rect, text: text});
     }
-    arr[arr.length - i - 1].rect.fill('#1abc9c'); // Set the last element to green
-  }
-}
 
-bubbleSort();
+    for (let i = arr.length - 1; i >= 1; i--) {
+      pos = i;
+      max = arr[i].value;
+      for (let j = i - 1; j >= 0; j--) {
+        // pink shows current max value
+        arr[pos].rect.fill("pink");
+        if (arr[j].value > max) {
+          arr[pos].rect.fill("#2ecc71");
+          pos = j;
+          max = arr[j].value;
+        };
+        arr[j].rect.fill("green");
+        await new Promise(r => setTimeout(r, 100)); // Pause for visualization
+        arr[j].rect.fill("#2ecc71");
+      }
+      arr[pos].rect.fill('#e74c3c');
+      await new Promise(r => setTimeout(r, 100)); // Pause for visualization
+
+      // Swap rectangles
+      const tempRectX = arr[pos].rect.x();
+      arr[pos].rect.x(arr[i].rect.x());
+      arr[i].rect.x(tempRectX);
+      
+      // Swap text
+      const tempTextX = arr[pos].text.x();
+      arr[pos].text.x(arr[i].text.x());
+      arr[i].text.x(tempTextX);
+      
+      // Swap elements in array
+      const temp = arr[pos];
+      arr[pos] = arr[i];
+      arr[i] = temp;
+      await new Promise(r => setTimeout(r, 100)); // Pause for visualization
+
+      // Color code the rectangles
+      //arr[i+1].rect.fill('#e74c3c');
+      arr[i].rect.fill('#1abc9c'); // Set the last element to green
+    };
+    arr[0].rect.fill("#1abc9c");
+  };
+
+
+  async function bubbleSort() {
+
+    const draw = SVG().addTo('#bubbleSort').size(600, 100);
+
+    const arr = [];
+    const rectWidth = 30;
+    const rectHeight = 30;
+    const rectSpacing = 10;
+    const rectStartX = 50;
+    const rectStartY = 50;
+
+    for (let i = 0; i < 14; i++) {
+      const randomNumber = Math.floor(Math.random() * 100);
+      const rect = draw.rect(rectWidth, rectHeight).move(rectStartX + i * (rectWidth + rectSpacing), rectStartY);
+      rect.fill('#2ecc71');
+      const text = draw.text(randomNumber).move(rectStartX + i * (rectWidth + rectSpacing) + 5, rectStartY + 7);
+      arr.push({value: randomNumber, rect: rect, text: text});
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j].value > arr[j+1].value) {
+          // Swap rectangles
+          const tempX = arr[j].rect.x();
+          arr[j].rect.x(arr[j+1].rect.x());
+          arr[j+1].rect.x(tempX);
+          
+          // Swap text
+          const tempTextX = arr[j].text.x();
+          arr[j].text.x(arr[j+1].text.x());
+          arr[j+1].text.x(tempTextX);
+          
+          // Swap elements in array
+          const temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+        // Color code the rectangles
+        arr[j].rect.fill('#2ecc71');
+        arr[j+1].rect.fill('#e74c3c');
+        await new Promise(r => setTimeout(r, 100)); // Pause for visualization
+      }
+      arr[arr.length - i - 1].rect.fill('#1abc9c'); // Set the last element to green
+    }
+  };
+
+  sortBySelection();
+  bubbleSort();
 
 
 });
