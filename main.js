c22a0fb77b3ae31cb32a5452d0c33617cded4931
Maxime Coens <maxime.coens@student.hogent.be>
2023-04-07 19:39:39 +0200

    sort by insertion

--- main.js
+++ main.js
@@ -54,12 +54,81 @@ window.addEventListener('load', () => {
       await new Promise(r => setTimeout(r, 100)); // Pause for visualization
 
       // Color code the rectangles
-      //arr[i+1].rect.fill('#e74c3c');
       arr[i].rect.fill('#1abc9c'); // Set the last element to green
     };
     arr[0].rect.fill("#1abc9c");
   };
 
+  async function insertionSort() {
+
+    const draw = SVG().addTo('#sortByInsertion').size(600, 100);
+
+    const arr = [];
+    const rectWidth = 30;
+    const rectHeight = 30;
+    const rectSpacing = 10;
+    const rectStartX = 50;
+    const rectStartY = 50;
+
+    for (let i = 0; i < 14; i++) {
+      const randomNumber = Math.floor(Math.random() * 100);
+      const rect = draw.rect(rectWidth, rectHeight).move(rectStartX + i * (rectWidth + rectSpacing), rectStartY);
+      rect.fill('#2ecc71');
+      const text = draw.text(randomNumber).move(rectStartX + i * (rectWidth + rectSpacing) + 5, rectStartY + 7);
+      arr.push({value: randomNumber, rect: rect, text: text});
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i]);
+    }
+
+    for (let i = 1; i <= arr.length - 1; i++) {
+      x = arr[i];
+      j = i;
+      while (j > 0 && x.value < arr[j - 1].value) {
+
+        // Swap rectangles
+        const tempX = arr[j].rect.x();
+        arr[j].rect.x(arr[j - 1].rect.x());
+        arr[j - 1].rect.x(tempX);
+        
+        // Swap text
+        const tempTextX = arr[j].text.x();
+        arr[j].text.x(arr[j - 1].text.x());
+        arr[j - 1].text.x(tempTextX);
+
+        // Swap elements in array
+        const temp = arr[j];          
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+
+        j--;
+      }
+      arr[j].value = x.value;
+      await new Promise(r => setTimeout(r, 100)); // Pause for visualization
+
+    }
+
+  };
+
+  async function mergeSort() {
+    const draw = SVG().addTo('#mergeSort').size(600, 100);
+
+    const arr = [];
+    const rectWidth = 30;
+    const rectHeight = 30;
+    const rectSpacing = 10;
+    const rectStartX = 50;
+    const rectStartY = 50;
+
+    for (let i = 0; i < 14; i++) {
+      const randomNumber = Math.floor(Math.random() * 100);
+      const rect = draw.rect(rectWidth, rectHeight).move(rectStartX + i * (rectWidth + rectSpacing), rectStartY);
+      rect.fill('#2ecc71');
+      const text = draw.text(randomNumber).move(rectStartX + i * (rectWidth + rectSpacing) + 5, rectStartY + 7);
+      arr.push({value: randomNumber, rect: rect, text: text});
+    }
+  }
 
   async function bubbleSort() {
 
@@ -108,9 +177,8 @@ window.addEventListener('load', () => {
   };
 
   sortBySelection();
+  insertionSort();
+  mergeSort();
   bubbleSort();
 
-
 });
-
-
