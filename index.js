749719a1ce4b7d22162480ce050cf746e706eb68
Thomas Louage <thomas.louage@student.hogent.be>
2023-05-19 19:05:23 +0200

    toevoegen van index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './sortAlgorithms.html';
+    }
+
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.html':
+            contentType = 'text/html';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+        default:
+            contentType = 'application/octet-stream';
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404);
+                res.end('File not found');
+            } else {
+                res.writeHead(500);
+                res.end('Server error');
+            }
+        } else {
+            res.writeHead(200, {
+                'Content-Type': contentType
+            });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server is running at http://localhost:3000/');
+});
